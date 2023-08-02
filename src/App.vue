390051c32410ba6d2985c3d8e5a3d356cea2849d
anuntawee <56260276+anuntawee@users.noreply.github.com>
2023-08-02 14:59:44 +0700

    build2

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
     <p>ที่อยู่ <span v-html="address"></span></p>
     <a :href="social">facebook</a>
     <p>งานอดิเรก</p> 
-    เรียกใช้งาน array
+    เรียกใช้งาน array test
     <ul>
       <li>{{hobby[0]}}</li>
       <li>{{hobby[1]}}</li>
